c1269f7d2905c06b5cbbab6d6180082e523cc7a9
jonimende <jonasyjere15@gmail.com>
2025-05-29 14:20:56 -0300

    fix :)

--- Server/src/Routes/exchangeRoutes.ts
+++ Server/src/Routes/exchangeRoutes.ts
@@ -4,6 +4,6 @@ import { authenticateToken } from "../Middlewares/authMiddleware";  // el middle
 
 const router = Router();
 
-router.post('/exchange', authenticateToken, exchangeCurrency);
+router.post('/', authenticateToken, exchangeCurrency);
 
 export default router;
